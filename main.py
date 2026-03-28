a1ec0b96da8e355d901cd335b4dae9e12e630823
c2t-r <80561604+c2t-r@users.noreply.github.com>
2026-03-28 21:16:57 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import json
+from pathlib import Path
+
+import requests
+
+OCTO_SERVER_DOMAIN = "resources-data.jp.chiikawa-pocket.com"
+RELEASE_ID_API = "https://resources-data.jp.chiikawa-pocket.com/tag/Prd_2.0.0"
+CATALOG_PATH = "catalog.json"
+OUT_PATH = "out"
+
+
+def main() -> None:
+    with open(CATALOG_PATH) as f:
+        catalog = json.load(f)
+
+    resp = requests.get(RELEASE_ID_API)
+    resp.raise_for_status()
+    release_id = resp.text
+
+    for bundle in catalog["asset_bundle"]:
+        if "http" not in bundle["load_path"]:
+            continue
+        url = (
+            bundle["load_path"]
+            .replace("OCTO_SERVER_DOMAIN", OCTO_SERVER_DOMAIN)
+            .replace("OCTO_SERVER_RELEASE_ID", release_id)
+        )
+        print(url)
+
+        resp = requests.get(url)
+        resp.raise_for_status()
+
+        out = OUT_PATH / Path("/".join(url.split("/")[-2:]))
+        out.parent.mkdir(parents=True, exist_ok=True)
+        with open(out, "wb") as f:
+            f.write(resp.content)
+
+
+if __name__ == "__main__":
+    main()
