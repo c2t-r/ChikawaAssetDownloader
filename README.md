7426b72026300bc981a47c22cdce1fa8a4bc9f9a
c2t-r <80561604+c2t-r@users.noreply.github.com>
2026-04-14 23:55:34 +0900

    feat: support latest catalog

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # ChikawaAssetDownloader
 
-> [!WARNING]
-> this repo is alpha version and not stable
-
 download chikawa asset  
-currently the version is hardcoded as `Prd_2.0.0`
+version is hardcoded as `Prd_2.0.1`
--- main.py
+++ main.py
@@ -1,36 +1,36 @@
-import json
 from pathlib import Path
 
 import requests
+from catalog import load_catalog_bytes
 
+VERSION = "Prd_2.0.1"
 OCTO_SERVER_DOMAIN = "resources-data.jp.chiikawa-pocket.com"
-RELEASE_ID_API = "https://resources-data.jp.chiikawa-pocket.com/tag/Prd_2.0.0"
-CATALOG_PATH = "catalog.json"
-OUT_PATH = "out"
+RELEASE_ID_API = f"https://resources-data.jp.chiikawa-pocket.com/tag/{VERSION}"
+OUT_PATH = Path("out")
 
 
 def main() -> None:
-    with open(CATALOG_PATH) as f:
-        catalog = json.load(f)
-
     resp = requests.get(RELEASE_ID_API)
     resp.raise_for_status()
-    release_id = resp.text  # 97fab255-29be-11f1-9800-0a58a9feac03
+    release_id = resp.text
 
-    for bundle in catalog["asset_bundle"]:
-        if "http" not in bundle["load_path"]:
+    url = f"https://{OCTO_SERVER_DOMAIN}/{release_id}/a/a/catalog.bin"
+    resp = requests.get(url)
+    resp.raise_for_status()
+    catalog = load_catalog_bytes(resp.content)
+
+    for bundle in catalog.asset_bundle:
+        if "http" not in bundle.load_path:
             continue
-        url = (
-            bundle["load_path"]
-            .replace("OCTO_SERVER_DOMAIN", OCTO_SERVER_DOMAIN)
-            .replace("OCTO_SERVER_RELEASE_ID", release_id)
+        url = bundle.load_path.replace("OCTO_SERVER_DOMAIN", OCTO_SERVER_DOMAIN).replace(
+            "OCTO_SERVER_RELEASE_ID", release_id
         )
         print(url)
 
         resp = requests.get(url)
         resp.raise_for_status()
 
-        out = OUT_PATH / Path("/".join(url.split("/")[-2:]))
+        out = OUT_PATH / "/".join(url.split("/")[-2:])
         out.parent.mkdir(parents=True, exist_ok=True)
         with open(out, "wb") as f:
             f.write(resp.content)
